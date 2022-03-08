6ad30e53275032348838654d7ce731a9d9b00c4d
Luka Piskorec <13149424+lukapiskorec@users.noreply.github.com>
2022-03-08 14:35:40 +0200

    mint test

---
+++ crystalyx/js/palettes/ProtocellsTextures.js
@@ -0,0 +1,222 @@
+/**
+ * @author @LukaPiskorec
+ */
+
+const ProtocellsTextures = {
+
+
+    'lapis lazuli' : {
+
+      'texture' : [
+
+        'lapis_006_1.png',
+        'lapis_006_2.png',
+        'lapis_006_3.png',
+        'lapis_006_4.png',
+        'lapis_006_5.png',
+        'lapis_006_6.png',
+
+        'lapis_007_1.png',
+        'lapis_007_2.png',
+        'lapis_007_3.png',
+        'lapis_007_4.png',
+        'lapis_007_5.png',
+        'lapis_007_6.png',
+
+        'lapis_008_1.png',
+        'lapis_008_2.png',
+        'lapis_008_3.png',
+        'lapis_008_4.png',
+        'lapis_008_5.png',
+        'lapis_008_6.png',
+
+        'lapis_009_1.png',
+        'lapis_009_2.png',
+        'lapis_009_3.png',
+        'lapis_009_4.png',
+        'lapis_009_5.png',
+        'lapis_009_6.png',
+
+        'lapis_010_1.png',
+        'lapis_010_2.png',
+        'lapis_010_3.png',
+        'lapis_010_4.png',
+        'lapis_010_5.png',
+        'lapis_010_6.png',
+
+        'lapis_020_1.png',
+        'lapis_020_2.png',
+        'lapis_020_3.png',
+        'lapis_020_4.png',
+        'lapis_020_5.png',
+        'lapis_020_6.png',
+
+        'lapis_030_1.png',
+        'lapis_030_2.png',
+        'lapis_030_3.png',
+        'lapis_030_4.png',
+        'lapis_030_5.png',
+        'lapis_030_6.png',
+
+        'lapis_040_1.png',
+        'lapis_040_2.png',
+        'lapis_040_3.png',
+        'lapis_040_4.png',
+        'lapis_040_5.png',
+        'lapis_040_6.png',
+
+        'lapis_050_1.png',
+        'lapis_050_2.png',
+        'lapis_050_3.png',
+        'lapis_050_4.png',
+        'lapis_050_5.png',
+        'lapis_050_6.png',
+
+        'lapis_060_1.png',
+        'lapis_060_2.png',
+        'lapis_060_3.png',
+        'lapis_060_4.png',
+        'lapis_060_5.png',
+        'lapis_060_6.png',
+
+        'lapis_070_1.png',
+        'lapis_070_2.png',
+        'lapis_070_3.png',
+        'lapis_070_4.png',
+        'lapis_070_5.png',
+        'lapis_070_6.png',
+
+        'lapis_080_1.png',
+        'lapis_080_2.png',
+        'lapis_080_3.png',
+        'lapis_080_4.png',
+        'lapis_080_5.png',
+        'lapis_080_6.png',
+
+        'lapis_090_1.png',
+        'lapis_090_2.png',
+        'lapis_090_3.png',
+        'lapis_090_4.png',
+        'lapis_090_5.png',
+        'lapis_090_6.png'
+
+      ],
+
+      'background' : [
+
+        ['#800080', 0.25], //full magenta
+        ['#640064', 0.25],
+        ['#320032', 0.25],
+        ['#160016', 0.25],
+        ['#006464', 0.25], //full cyan
+        ['#004848', 0.25],
+        ['#003232', 0.25],
+        ['#0000ff', 0.25], //full blue
+        ['#000096', 0.25],
+        ['#000064', 0.25],
+        ['#000032', 0.25],
+        ['#000016', 0.25],
+        ['#646400', 0.25], //full yellow
+        ['#404000', 0.25],
+        ['#480096', 0.25],
+        ['#320064', 0.25],
+        ['#160032', 0.25],
+        ['#003264', 0.25],
+        ['#001632', 0.25],
+        ['#640032', 0.25],
+        ['#320016', 0.25],
+        ['#006432', 0.25],
+        ['#003216', 0.25]
+
+      ],
+
+    },
+
+
+
+
+
+    'ruby' : {
+
+      'texture' : [
+
+        'lapis_130_1.png',
+        'lapis_130_2.png',
+        'lapis_130_3.png',
+        'lapis_130_4.png',
+        'lapis_130_5.png',
+        'lapis_130_6.png',
+        'lapis_130_7.png',
+
+      ],
+
+      'background' : [
+
+        ['#640000', 0.25],
+
+      ],
+
+    },
+
+
+
+
+    'emerald' : {
+
+      'texture' : [
+
+        'lapis_260_1.png',
+        'lapis_260_2.png',
+        'lapis_260_3.png',
+        'lapis_260_4.png',
+        'lapis_260_5.png',
+        'lapis_260_6.png',
+        'lapis_260_7.png',
+
+      ],
+
+      'background' : [
+
+        ['#003200', 0.25],
+
+      ],
+
+    },
+
+
+
+};
+
+
+//800080 - full magenta
+//640064
+//320032 - dark magenta
+//160016
+//080008
+
+//006464 - full cyan
+//003232 - dark cyan
+
+//0000ff - full blue
+//000096
+//000064
+//000032
+//000016 - dark blue
+//000008
+
+//646400 - full yellow
+
+//480096 - violet
+//320064
+//160032 - dark purple
+//080016
+
+//003264 - dark cyan / blue
+//001632
+
+//640032
+//320016 - dark magenta / red
+//160008
+
+//006432
+//003216 - dark cyan / green
